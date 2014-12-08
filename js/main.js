96c7d8ee806206eab180018f802970ca2d78bf12
thomasrand <trand87@gmail.com>
2014-12-08 01:21:13 -0500

    "stuff"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function handle_primary_CTA() {
     $('#primary-cta').click(function(evt) {
         evt.stopImmediatePropagation();
-        
-        $('body').scrollTop($('#services').position().top);
+
+        $('body').animate({scrollTop:$('#services').position().top}, 'slow');
     });
 }
 
@@ -14,8 +14,8 @@ function navigate_to() {
         var target_section_name = $(this).attr('link-to');
         var target_section = $('#' + target_section_name);
         var section_loc = target_section.position().top;
-        
-        $('body').scrollTop(section_loc);
+
+        $('body').animate({scrollTop:section_loc}, 'slow');
     });
 };
 
@@ -65,6 +65,18 @@ function set_modal_bkg_size(modal) {
     $(modal).find('.modal-bkg').css('height', modal_size + $(window).height()*0.1 + 40 + 'px');
 }
 
+function initalizeScrolltoFixed() {
+    $('header').scrollToFixed({
+        unfixed: function() {
+            $(this).css('background', 'rgba(255,255,255,.25'); 
+            $(this).find('li').css('color', '#FFFFFF');
+        },
+        fixed: function() {
+            $(this).css('background', 'rgba(255,255,255,.75'); 
+            $(this).find('li').css('color', 'rgba(0,0,0,.87)');
+        }
+    });
+}
 
 
 $(function initalize() {
@@ -72,4 +84,5 @@ $(function initalize() {
     close_modal();
     navigate_to();
     handle_primary_CTA();
+    initalizeScrolltoFixed();
 });
