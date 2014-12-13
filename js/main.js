9f0b6f1ec06919c60afe73c383dfc0f2401e8926
Tom Rand <trand87@gmail.com>
2014-12-13 17:20:31 -0500

    weddding site

--- js/main.js
+++ js/main.js
@@ -1,88 +1,105 @@
-function handle_primary_CTA() {
-    $('#primary-cta').click(function(evt) {
-        evt.stopImmediatePropagation();
+//  Set the height of the header to the height of the video player 
+function setIntroVideoSize() {
+	var videoHeight = $('#intro-video').height();
 
-        $('body').animate({scrollTop:$('#services').position().top}, 'slow');
-    });
+    var tabsHeight = 59;
+    var windowHeight = window.innerHeight;
+	
+    $('.header').css('max-height', (windowHeight - tabsHeight) + 'px'); 
+    $('.header').css('height', videoHeight + 'px');
+    $('.overlay').css('height', videoHeight + 'px');
+
+    var headerHeight = $('.header').height();
+
+    $('article').css('top', headerHeight + 'px'); 
+}
+
+//  Adjust the header when the browser width is changed
+function adjustHeaderOnResize() {
+    "use strict";
+    
+	$(window).resize(function () {
+		setIntroVideoSize();
+	});
 }
 
 
-function navigate_to() {
-    $('.primary-navigation li').click(function(evt) {
-        evt.stopImmediatePropagation();
+
+function tabEventHandler() {
+    "use strict";
+	$('nav li').click(function (evt) {
         
-        var target_section_name = $(this).attr('link-to');
-        var target_section = $('#' + target_section_name);
-        var section_loc = target_section.position().top;
+        evt.stopImmediatePropagation();
+        var section = $(this).find('a').attr('class');
+
+        var section_loc = $('.contents').find('.' + section).offset().top -59;
 
         $('body').animate({scrollTop:section_loc}, 'slow');
-    });
-};
 
-function open_modal() {
-    $('.open-modal').click(function(evt) {
-        evt.stopImmediatePropagation();
-        
-        var target_modal_name = $(this).attr('target-modal');
-        var target_modal = $('#' + target_modal_name);  
-        
-        $(target_modal).fadeIn();
-        $('body').addClass('no-scroll');
-        
-        // set_modal_max_height(target_modal);
-        set_modal_bkg_size(target_modal);
-        
-    });
-};
+	});
+}
 
-function close_modal() {
-    $('.close-icon').click(function(evt) {
+function linkEventHandler() {
+    $('.splash-letter span').click(function (evt) {
         evt.stopImmediatePropagation();
         
-        $('.modal').fadeOut();
-        $('body').removeClass('no-scroll');
-    });
-    
-    $('.modal-bkg').click(function(evt) {
-        evt.stopImmediatePropagation();
-        
-        $('.modal').fadeOut();
-        $('body').removeClass('no-scroll');
-            
+
     });
 }
 
-function set_modal_max_height(modal) {
-    var window_size = $(window).height();
-    var modal_header_size = $(modal).find('.modal-header').height();
-    var max_modal_height = window_size - ((window_size*0.1) + modal_header_size + 180);
-    
-    $(modal).find('.modal-body').css('max-height', max_modal_height);
-}
-
-function set_modal_bkg_size(modal) {
-    var modal_size = $(modal).find('.modal-wrapper').height();
-    $(modal).find('.modal-bkg').css('height', modal_size + $(window).height()*0.1 + 40 + 'px');
-}
-
-function initalizeScrolltoFixed() {
-    $('header').scrollToFixed({
-        unfixed: function() {
-            $(this).css('background', 'rgba(255,255,255,.25'); 
-            $(this).find('li').css('color', 'rgba(0,0,0,.87)');
-        },
+function stickyNav() {  
+    $('.tabs').scrollToFixed({
+        zIndex : 1000000,
         fixed: function() {
-            $(this).css('background', 'rgba(255,255,255,.75'); 
-            $(this).find('li').css('color', 'rgba(0,0,0,.87)');
+            $('.splash-letter').css('visibility', 'hidden');
+            $('.header').css('overflow', 'visible');
+        },
+        unfixed: function() {
+            $('.splash-letter').css('visibility', 'visible');
+            $('.header').css('overflow', 'hidden');
         }
     });
+
+    /*
+    var stickyNavTop = $('nav').offset().top;
+
+    var stickyNav = function(){  
+        var scrollTop = $(window).scrollTop();  
+        
+        
+        if (scrollTop > stickyNavTop) {   
+            $('nav').addClass('sticky');  
+        } else {  
+            $('nav').removeClass('sticky');   
+        }  
+    };  
+  
+    stickyNav();  
+
+    $(window).scroll(function() {  
+        stickyNav();  
+    });  
+*/
 }
+/*
+function loadRSVPModal {
+
+require(["mojo/signup-forms/Loader"], function(L) { 
+    L.start({"baseUrl":"mc.us5.list-manage.com","uuid":"f99ffe693efb9c8d734e6507c","lid":"e226bfe8bb"}) })
 
 
-$(function initalize() {
-    open_modal();
-    close_modal();
-    navigate_to();
-    handle_primary_CTA();
-    initalizeScrolltoFixed();
+}
+*/
+
+
+$(function initilize() {
+    "use strict";
+	window.setTimeout(function() {setIntroVideoSize();}, 300); 
+    window.setTimeout(function() {stickyNav();}, 500); 
+	adjustHeaderOnResize();
+	tabEventHandler();
+    linkEventHandler();
 });
+
+
+
