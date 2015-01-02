97ab409569f2fd87d76db9f6e99772742d7ec490
thomasrand <trand87@gmail.com>
2015-01-01 21:46:56 -0500

    added background image and grammar updates.

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ function setIntroVideoSize() {
 
 //  Adjust the header when the browser width is changed
 function adjustHeaderOnResize() {
-    "use strict";
     
 	$(window).resize(function () {
 		setIntroVideoSize();
@@ -26,7 +25,6 @@ function adjustHeaderOnResize() {
 
 
 function tabEventHandler() {
-    "use strict";
 	$('nav li').click(function (evt) {
         
         evt.stopImmediatePropagation();
@@ -60,47 +58,25 @@ function stickyNav() {
         fixed: function() {
             $('.splash-letter').css('visibility', 'hidden');
             $('.header').css('overflow', 'visible');
+            
+            $('.header').css('background-image', 'url("assets/bg-image.png")');  
+            $('.header').addClass('header-alternate');
+            $('#intro-video').css('visibility', 'hidden');
         },
         unfixed: function() {
             $('.splash-letter').css('visibility', 'visible');
             $('.header').css('overflow', 'hidden');
+
+            $('.header').css('background-image', 'none');  
+            $('.header').removeClass('header-alternate');
+            $('#intro-video').css('visibility', 'visible');
         }
     });
+}  
 
-    /*
-    var stickyNavTop = $('nav').offset().top;
-
-    var stickyNav = function(){  
-        var scrollTop = $(window).scrollTop();  
-        
-        
-        if (scrollTop > stickyNavTop) {   
-            $('nav').addClass('sticky');  
-        } else {  
-            $('nav').removeClass('sticky');   
-        }  
-    };  
-  
-    stickyNav();  
-
-    $(window).scroll(function() {  
-        stickyNav();  
-    });  
-*/
-}
-/*
-function loadRSVPModal {
-
-require(["mojo/signup-forms/Loader"], function(L) { 
-    L.start({"baseUrl":"mc.us5.list-manage.com","uuid":"f99ffe693efb9c8d734e6507c","lid":"e226bfe8bb"}) })
-
-
-}
-*/
 
 
 $(function initilize() {
-    "use strict";
 	window.setTimeout(function() {setIntroVideoSize();}, 300); 
     window.setTimeout(function() {stickyNav();}, 500); 
 	adjustHeaderOnResize();
